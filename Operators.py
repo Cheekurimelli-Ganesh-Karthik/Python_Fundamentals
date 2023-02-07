d9e6a639639ecfb46277e716f4eb2e197ca7f968
Ganesh_Karthik <124451151+GANESH-KARTHIK-CH@users.noreply.github.com>
2023-02-07 17:45:23 +0000

    Operators in python

---
+++ Operators.py
@@ -0,0 +1,28 @@
+#OPERATORS
+
+#1.ARTHMETIC OPERATORS#
+
+print(10+5)
+float1=10.23
+float2=52.31
+print(float1+float2)
+print(float1-float2)
+print(float1-5)
+print(float1*float2)
+print(float1*3)
+print(float1/float2)
+print(float1/2)
+print(float1//float2)
+print(-28 % 10)  # The remainder is positive if the right-hand operand is positive
+print(28 % -10)  # The remainder is negative if the right-hand operand is negative
+print(34.4 % 2.5)  # The remainder can be a float
+
+#2.COMPARISON OPERATORS
+A=5
+B=10
+C=10
+L1=[6,7,8]
+L2=[6,7,8]
+print(A>B)
+print(B>A)
+print(B==C)
